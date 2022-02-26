d85f78bd8111ae48f30adf830e11718c599cc7c0
hirofumi_i <detroit69city.nau@gmail.com>
2022-02-26 17:10:31 +0900

    Change the directory of the Code component

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -4,8 +4,8 @@ type CodeProps = {
   language: string;
   children: string;
 }
-const Code = (props: CodeProps) => {
-  return <CodePane language={props.language}>{props.children}</CodePane>;
+const Code = ({language, children}: CodeProps) => {
+  return <CodePane language={language}>{children}</CodePane>;
 };
 
 export default Code;
